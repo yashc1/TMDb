a01bd266dfc6b12b648b338bcfebe1d65b56ee38
janam13 <32513449+janam13@users.noreply.github.com>
2017-11-04 16:23:32 +0530

    Add files via upload

---
+++ person.sql
@@ -0,0 +1,81 @@
+CREATE TABLE Person
+(
+  PersonID INT NOT NULL,
+  Name VARCHAR(20) NOT NULL,
+  Gender VARCHAR(1) NOT NULL,
+  BirthDate DATE NOT NULL,
+  EmailID VARCHAR(30) NOT NULL,
+  PRIMARY KEY (PersonID),
+  UNIQUE (EmailID)
+);
+insert all
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Dhruv Saraiya','dhruvsaraiya@gmail.com','M','12-jan-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Dhruvesh Rajodiya','dhruveshrajodiya@gmail.com','M','11-feb-198');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Pranay Shah','pranayshah@gmail.com', 'M','07-jul-1998');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Parashar Parikh','parasharparikh@gmail.com','M','18-jan-1998');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Vnit Pandya','vinitpamdya@gmail.com', 'M','26-aug-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Siddhant Singhal','siddhantsinghal@gmail.com','M','15-jun-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Srinjay Dan','srinjaydan@gmail.com','M','21-mar-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Krupal Patel','krupalpatel@gmail.com','M','19-nov-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Rohan Rudani','rohanrudani@gmail.com','M','11--1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Monish Shah','monishshah@gmail.com','M','18-apr-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Nishant Atal','nishantatal@gmail.com','M','17-jun-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Akshay Mehta','akshaymehta@gmail.com','M','27-nov-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Pratham Mistry','prathammistry@gmail.com','M','29-sep-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Jaydev Patel','jaydevpatel@gmail.com','M','20-nov-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Krunal Katrodiya','krunalkatrodiya@gmail.com','M','01-dec-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Jhanvi Chanababa','jhanvichanababa@gmail.com','F','08-sep-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Harima Patel','harimapatel@gmail.com','F','30-oct-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Anushka Nene','anushkanene@gmail.com','F','31-dec-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Harsh Shah','harshshah@gmail.com','M','17-jun-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Jeel Patel','jeelpatel@gmail.com','M','10-apr-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Leonardo DiCaprio','leonardodicaprio@gmail.com','M','11-nov-1974');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Kate Winslet','katewinslet@gmail.com','F','5-Oct-1975');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Christian Bale','christianbale@gmail.com','M','30-Jan-1974');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Heath Ledger','heathledger@gmail.com','M','4-Apr-1979');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Patrick Wilson','patrickwilson@gmail.com','M','3-Jul-1973');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Vera Farmiga','verafarmiga@gmal.com','F','6-Aug-1973');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Sam Worthington','samworthington@gmail.com','M','2-Aug-1976');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Matt Damon','mattdamon@gmail.com','M','8-Oct-1970');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Mark Wahlberg','markwahlberg@gmail.com','M','5-jun-1973');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Salman Khan','salmankhan@gmail.com','M','27-Dec-1965');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Kareena Kapoor','kareenakapoor@gmail.com','F','21-sep-1980');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Aamir Khan','aamirkhan@gmail.com','M','14-Mar-1965');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Anushka Sharma','anushkasharma@gmial.com','F',' 1-May-1988');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Shah Rukh Khan','shahrukhkhan@gmail.com','M','2-Nov-1965');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Deepika Padukone','deepikapadukone@gmail.com','F','5-Jan-1986');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Akshay Kumar','akshaykumar@gmail.com','M','9-Sep-1967');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Ileana DCruz','ileanadcruz@gmail.com','F','1-Nov-1987');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Hrithik Roshan','hrithikroshan@gamil.com','M','10-jan-1974');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Priyanka Chopra','priyankachopra@gmail.com','F','18-Jul-1982 ');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Emilia Clarke','emiliaclarke@gmail.com','F','23-Oct-1986');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Kit Harington','kitharington@gmail.com','M','26-Dec-1986');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Lena Headey','lenaheadey@gmail.com','F','3-Oct-1973');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Peter Dinklage','peterdinklage@gmail.com','M','11-Jun-1969');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Nikolaj Coster-Waldau','nikolajcoster-waldau@gmail.com','M','27-Jul-1970');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Maisie Williams','maisiewilliams@gmail.com','F','15-Apr-1997');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Sophie Turner','sophieturner@gmail.com','F','21-Feb-1996');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Bryan Cranston','bryancranston@gmail.com','M','7-Mar-1956');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Aaron Paul','aaronpaul@gmail.com','M','27-Aug-1979');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Anna Gunn','annagunn@gmaidcom','F','11-Aug-1968');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Dean Norris','deannorris@gmail.com','M','8-Apr-1963');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Kaley Cuoco','kaleycuoco@gmail.com','F','30-Nov-1985');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Johnny Galecki','johnnygalecki@gmail.com','M','30-Apr-1975');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Jim Parsons','jimparsons@gmail.com','M','24-Mar-1973');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Simon Helberg','simonhelberg@gmail.com','M','9-dec-1980');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Kunal Nayyar','kunalnayyar@gmail.com','M','30-Apr-1981');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Mayim Bialik','mayimbialik@gmail.com','F','12-Dec-1975');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Melissa Rauch','melissarauch@gmail.com','F','23-Jun-1980');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Benedict Cumberbatch','benedictcumberbatch@gmail.com','M','19-Jul-1976');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Martin Freeman','martinfreeman@gmail.com','M','8-Sep-1971');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Andrew Scott','andrewscott@gmail.com','M','21-oct-1976');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Jennifer Aniston','jenniferaniston@gmail.com','F','11-Feb-1969');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Courteney Cox','courteneycox@gmail.com','F','15-Jun-1964');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Lisa Kudrow','lisakudrow@gmail.com','F','30-Jul-1963');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Matthew Perry','matthewperry@gmail.com','M','19-Aug-1969');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Matt LeBlanc','mattleBlanc@gmail.com','M','25-jul-1967');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'David Schwimmer','davidschwimmer@gmail.com','M','2-Nov-1966');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Kevin Spacey','kevinspacey@gmail.com','M','26-jul-1959');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Michael Kelly','michaelkelly@gmail.com','M','22-May-1968');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Robin Wright','robinwright@gmail.com','F','8-Apr-1966');
+insert into Person (PersonID, Name, EmailID, Gender, BirthDate) values (Person_sequence.nextval,'Kate Mara','katemara@gmail.com','F','27-Feb-1983');
