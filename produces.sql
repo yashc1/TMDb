bec8983abea9b977c6b9a99138bac619026b0e2d
janam13 <32513449+janam13@users.noreply.github.com>
2017-11-04 14:41:27 +0530

    Delete produces.sql

--- produces.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Produces
-(
-  MediaID INT NOT NULL,
-  StudioID INT NOT NULL,
-  PRIMARY KEY (MediaID, StudioID),
-  FOREIGN KEY (MediaID) REFERENCES Media(MediaID),
-  FOREIGN KEY (StudioID) REFERENCES Studio(StudioID)
-);
-insert into Produces (MediaID,StudioID) values ( , );
